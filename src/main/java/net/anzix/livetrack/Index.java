2c597b8383378d88b521138b17b78364dee657c9
Elek, Marton <level2@anzix.net>
2013-05-14 21:55:26 +0200

    index page with generated urls

---
+++ src/main/java/net/anzix/livetrack/Index.java
@@ -0,0 +1,40 @@
+package net.anzix.livetrack;
+
+import com.google.inject.Inject;
+import com.google.sitebricks.At;
+import com.google.sitebricks.http.Get;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Starting page.
+ */
+@At("/")
+public class Index {
+
+    @Inject
+    HttpServletRequest request;
+
+    private String url;
+
+    @Get
+    public void get(){
+        this.url = request.getRequestURL().toString();
+    }
+
+    public HttpServletRequest getRequest() {
+        return request;
+    }
+
+    public void setRequest(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
