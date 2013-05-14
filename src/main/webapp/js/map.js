f263fccbd6af0b1387bc37bcd4eaf3719bb21c3d
Elek, Marton <level2@anzix.net>
2013-05-14 22:59:58 +0200

    switching to leaflets

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -3,98 +3,87 @@ var markers = {};
 var popups = {};
 var markerLayer
 
-function createMarker(id, center, updated) {
-    var size = new OpenLayers.Size(21,25);
-    var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-    var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
-    marker = new OpenLayers.Marker(center,icon);
-    var popup = new OpenLayers.Popup("Popup",
-				     center,
-				     new OpenLayers.Size(350,60),
-				     '<div>TODO</div>',
-				     false);
-    map.addPopup(popup);
-    popup.hide();
-    //here add mouseover event
-    marker.events.register('mouseover', marker, function(evt) {
-	   popup.show();
-    });
 
-    //here add mouseout event
-    marker.events.register('mouseout', marker, function(evt) {popup.hide();});
-    marker.display(false);
-    markerLayer.addMarker(marker);
+var map;
+function initMap() {
+   map  = L.map('map');
+   var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+   var osmAttrib='Map data © OpenStreetMap contributors';
+   var osm = new L.TileLayer(osmUrl, {minZoom: 6    , maxZoom: 15, attribution: osmAttrib});
+   map.addLayer(osm);
+   map.setView([51.505, -0.09], 13);
+
+   var refresh = function(id, lon,lat,updated){
+           updateMarker(id, lon,lat, updated)
+
+       }
+
+       context = document.location.pathname.substring(0,document.location.pathname.search("map")-1)
+       if (context.length == 0) {
+          context = "/"
+       }
+       sock = new sitebricks.Channel(context);
+       sock.connect();
+
+       sock.on('message', function(data) {
+          console.log(data)
+          lastData = JSON.parse(JSON.parse(data));
+          console.log(lastData)
+          refresh(lastData.id,lastData.point.lon,lastData.point.lat, new Date(lastData.point.date))
+
+       });
+       sock.on('connect', function() {
+         console.log('websocket connected!');
+         sock.send(null, "SUB " + sitebricks.SOCKET_ID +",qwe")
+       });
+       sock.on('disconnect', function() {
+         console.log('websocket disconnected!');
+       });
+}
+
+function fixContentHeight() {
+    var footer = $("div[data-role='footer']:visible"),
+    content = $("div[data-role='content']:visible:visible"),
+    viewHeight = $(window).height(),
+    contentHeight = viewHeight - footer.outerHeight();
+
+    if ((content.outerHeight() + footer.outerHeight()) !== viewHeight) {
+        contentHeight -= (content.outerHeight() - content.height() + 1);
+        content.height(contentHeight);
+        $("#map").height(contentHeight);
+    }
+
+    if (map) {
+        map.invalidateSize();
+    } else {
+        // initialize map
+        initMap()
+    }
+}
+
+$('#mappage').live('pageshow',function (){
+    fixContentHeight();
+});
+
+$(window).bind("orientationchange resize pageshow", fixContentHeight);
+
+function createMarker(id, lon, lat, updated) {
+    marker = L.marker([lat,lon])
     markers[id] = marker;
-    popups[id] = popup;
+    marker.addTo(map);
 }
 
-function updateMarker(id, lonLat, updated) {
+function updateMarker(id, lon, lat, updated) {
         if (!markers[id]) {
-           createMarker(id,lonLat, updated)
+           map.setView([lat,lon],15)
+           createMarker(id,lon,lat, updated)
         }
-        var popup = popups[id]
+
         var marker = markers[id]
-	    popup.moveTo(map.getLayerPxFromLonLat(lonLat))
-	    marker.moveTo(map.getLayerPxFromLonLat(lonLat))
-	    popup.setContentHTML("<center><p>id: " + id + "</p><p>updated: "+updated.toISOString()+"</p></center>");
-	    map.setCenter (lonLat, map.getZoom());
-	    marker.display(true);
-	    markerLayer.redraw()
+        marker.bindPopup("<p>id: " + id + "</p>");
+        marker.setLatLng([lat,lon])
 
 }
 
 
-function createMap(key){
-    map = new OpenLayers.Map("map");
-    map.addLayer(new OpenLayers.Layer.OSM());
-    markerLayer = new OpenLayers.Layer.Markers( "Position" );
-    map.addLayer(markerLayer);
 
-
-
-
-
-
-    var center = new OpenLayers.LonLat(0,0).transform(
-        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-        map.getProjectionObject() // to Spherical Mercator Projection
-    );
-
-
-    
-    map.setCenter (center, 12);
-
-
-    
-
-    var refresh = function(id, lon,lat,updated){
-        var lonLat = new OpenLayers.LonLat(lon, lat);
-        lonLat = lonLat.transform(new OpenLayers.Projection("EPSG:4326"),map.getProjectionObject());
-        updateMarker(id, lonLat, updated)
-
-    }
-
-    context = document.location.pathname.substring(0,document.location.pathname.search("map")-1)
-    if (context.length == 0) {
-       context = "/"
-    }
-    sock = new sitebricks.Channel(context);
-    sock.connect();
-
-    sock.on('message', function(data) {
-       console.log(data)
-       lastData = JSON.parse(JSON.parse(data));
-       console.log(lastData)
-       refresh(lastData.id,lastData.point.lon,lastData.point.lat, new Date(lastData.point.date))
-
-    });
-    sock.on('connect', function() {
-      console.log('websocket connected!');
-      sock.send(null, "SUB " + sitebricks.SOCKET_ID +",qwe")
-    });
-    sock.on('disconnect', function() {
-      console.log('websocket disconnected!');
-    });
-
-    
-}
